b628487d419de28884e753a95ec034b144ec02a6
Syafiq Fajrian Emha <syafiqfajrianemha@gmail.com>
2023-05-25 05:35:03 +0700

    Latihan mengirim data dengan Parcelable

--- app/src/main/java/com/example/myintentapp/MainActivity.kt
+++ app/src/main/java/com/example/myintentapp/MainActivity.kt
@@ -16,6 +16,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         val btnMoveWithDataActivity: Button = findViewById(R.id.btn_move_activity_data)
         btnMoveWithDataActivity.setOnClickListener(this)
+
+        val btnMoveWithObject: Button = findViewById(R.id.btn_move_activity_object)
+        btnMoveWithObject.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
@@ -30,6 +33,19 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "Syafiq Fajrian Emha")
                 moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 19)
                 startActivity(moveWithDataIntent)
+            }
+
+            R.id.btn_move_activity_object -> {
+                val person = Person(
+                    "Syafiq Fajrian Emha",
+                    19,
+                    "syafiqfajrianemha@gmail.com",
+                    "Purbalingga Central Java"
+                )
+                
+                val moveWithObjectIntent = Intent(this@MainActivity, MoveWithObjectActivity::class.java)
+                moveWithObjectIntent.putExtra(MoveWithObjectActivity.EXTRA_PERSON, person)
+                startActivity(moveWithObjectIntent)
             }
         }
     }
